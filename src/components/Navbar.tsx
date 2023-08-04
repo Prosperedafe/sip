84ca8fae39255709c2265233d6f2aeb89ffdd245
prosper edafe <mamusedafe19@gmail.com>
2023-08-04 13:44:12 +0100

    base files added

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,21 @@
+import { CartIcon, MainLogo, SearchIcon } from "./icons/MainLogo";
+import { NavLink } from "react-router-dom";
+
+export const Navbar = () => {
+    return (
+        <header className="main__header">
+            <nav className="main__header__nav flex space-between items-center">
+                <MainLogo />
+                <div className="links flex items-center">
+                    <NavLink className={({ isActive }) => (isActive ? "active" : "")} to="/">Home</NavLink>
+                    <NavLink to="/blog">Blog</NavLink>
+                    <NavLink to="/cart" className="cart items-center">
+                        <CartIcon />
+                        <span>Cart</span>
+                    </NavLink>
+                    <SearchIcon />
+                </div>
+            </nav>
+        </header >
+    )
+}
---
+++ src/pages/Homepage/HomePage.tsx
@@ -0,0 +1,7 @@
+const HomePage = () => {
+    return (
+        <section id="home__page">hfvjrw</section>
+    )
+}
+
+export default HomePage;
