5242c8b284b0582756a7f19dcc3e09275f489277
prosper edafe <mamusedafe19@gmail.com>
2023-09-05 05:27:36 +0100

    adjusted ui

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import HomePage from './pages/Homepage/HomePage';
 import SavedItems from './pages/account/saved';
 import EditAccount from './pages/account/edit';
 import FruitRecipe from './pages/fruitRecipe/recipe';
+import ScrollToTop from './components/ScrollToTop';
 import AccountOrders from './pages/account/orders';
 import AccountOverview from './pages/account/overview';
 import { Footer } from './components/Footer';
@@ -14,24 +15,26 @@ import { Navbar } from './components/Navbar';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
-  const checkStorage = JSON.parse(localStorage.getItem("fruit__un-iD") as string);
   return (
     <BrowserRouter>
       <Navbar />
-      <Routes>
-        <Route path='/' element={<HomePage />} />
-        <Route path='/blog' element={<Blog />} />
-        <Route path='/contact' element={<Contact />} />
-        <Route path='/login' element={<Login />} />
-        <Route path='/signup' element={<SignUp />} />
-        <Route path='/account/overview' element={<AccountOverview />} />
-        <Route path='/account/saved' element={<SignUp />} />
-        <Route path='/account/orders' element={<AccountOrders />} />
-        <Route path='/account/management' element={<SignUp />} />
-        <Route path='/account/saved-items' element={<SavedItems />} />
-        <Route path='/account/edit-profile/:id' element={<EditAccount />} />
-        {checkStorage !== null ? <Route path='blog/recipe/:fruit' element={<FruitRecipe />} /> : null}
-      </Routes>
+      <ScrollToTop />
+      <main>
+        <Routes>
+          <Route path='/' element={<HomePage />} />
+          <Route path='/blog' element={<Blog />} />
+          <Route path='/contact' element={<Contact />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/signup' element={<SignUp />} />
+          <Route path='/account/overview' element={<AccountOverview />} />
+          <Route path='/account/saved' element={<SignUp />} />
+          <Route path='/account/orders' element={<AccountOrders />} />
+          <Route path='/account/management' element={<SignUp />} />
+          <Route path='/account/saved-items' element={<SavedItems />} />
+          <Route path='/account/edit-profile/:id' element={<EditAccount />} />
+          <Route path='blog/recipe/:fruit' element={<FruitRecipe />} />
+        </Routes>
+      </main>
       <Footer />
     </BrowserRouter>
   )
