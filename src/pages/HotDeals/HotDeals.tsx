2e2f8c9c11f963176d3e3bc8262bde16e215f038
prosper edafe <mamusedafe19@gmail.com>
2023-09-24 23:49:55 +0100

    deals page completed

--- src/pages/HotDeals/HotDeals.tsx
+++ src/pages/HotDeals/HotDeals.tsx
@@ -1,3 +1,6 @@
+import { Categories } from "../../components/products/categories";
+import { hotDeals, mostSearched, recentlyViewed } from "../Homepage/data";
+
 const DealOfTheDay = () => {
     return (
         <>
@@ -5,6 +8,11 @@ const DealOfTheDay = () => {
                 <section id="deals__day__hero">
                     <h2>Deals of the Day</h2>
                     <p>Get 50% off Juice purchases today</p>
+                </section>
+                <section id="deals__day__deals">
+                    <Categories products={hotDeals} />
+                    <Categories products={recentlyViewed} />
+                    <Categories products={mostSearched} />
                 </section>
             </section>
         </>
