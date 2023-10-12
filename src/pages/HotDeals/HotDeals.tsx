44fbf2d8ef28c6362e91debece7923788139b03a
prosper edafe <mamusedafe19@gmail.com>
2023-10-12 18:51:10 +0100

    fixed deploy error

--- src/pages/HotDeals/HotDeals.tsx
+++ src/pages/HotDeals/HotDeals.tsx
@@ -1,7 +1,29 @@
 import { Categories } from "../../components/products/categories";
 import { hotDeals, mostSearched, recentlyViewed } from "../Homepage/data";
+import { Product } from "../../schema/interface";
 
 const DealOfTheDay = () => {
+    const deals: Product[] = hotDeals.map((productData: any) => ({
+        id: productData.id,
+        name: productData.name,
+        price: productData.price,
+        rating: productData.rating,
+        drink: productData.drink,
+    }));
+    const searched: Product[] = mostSearched.map((productData: any) => ({
+        id: productData.id,
+        name: productData.name,
+        price: productData.price,
+        rating: productData.rating,
+        drink: productData.drink,
+    }));
+    const viewed: Product[] = recentlyViewed.map((productData: any) => ({
+        id: productData.id,
+        name: productData.name,
+        price: productData.price,
+        rating: productData.rating,
+        drink: productData.drink,
+    }));
     return (
         <>
             <section id="deals__day">
@@ -10,9 +32,9 @@ const DealOfTheDay = () => {
                     <p>Get 50% off Juice purchases today</p>
                 </section>
                 <section id="deals__day__deals">
-                    <Categories products={hotDeals} />
-                    <Categories products={recentlyViewed} />
-                    <Categories products={mostSearched} />
+                    <Categories products={deals} />
+                    <Categories products={viewed} />
+                    <Categories products={searched} />
                 </section>
             </section>
         </>
